8b07eb7be7b574256995da4fd1a7191c793619fd
rehmatkarim <p146120@nu.edu.pk>
2021-01-11 20:58:49 +0500

    seasons display

---
+++ src/SeasonDisplay.js
@@ -0,0 +1,24 @@
+import React from 'react'
+const getSeason=(lat,month) =>{
+    if (month > 2 && month < 9){
+         return lat > 0 ? 'Summer' : 'Winter';
+    }
+    else {
+        return lat > 0 ? 'Winter' : 'Summer';
+
+    }
+
+
+}
+
+const SeasonDisplay = props => {
+    const season = getSeason(props.lat, new Date().getMonth());
+    console.log(season);
+    return (
+        <div>
+            <h1>Season Display</h1>
+        </div>
+    )
+}
+
+export default SeasonDisplay;   
